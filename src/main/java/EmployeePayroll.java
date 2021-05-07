42822f87c2588fe527793c0c41aa3f9af20cd65a
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-05-07 11:02:38 +0530

    ADD: welcome note

---
+++ src/main/java/EmployeePayroll.java
@@ -0,0 +1,9 @@
+import java.sql.*;
+import java.util.Enumeration;
+
+public class EmployeePayroll {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll Database");
+    }
+}
