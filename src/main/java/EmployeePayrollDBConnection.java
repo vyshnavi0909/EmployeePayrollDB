26288c48bda880afdebf4706765bc75e4e1712c1
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-05-16 13:11:49 +0530

    ADD: prepared statement to update salary of particular employee

--- src/main/java/EmployeePayrollDBConnection.java
+++ src/main/java/EmployeePayrollDBConnection.java
@@ -14,6 +14,11 @@ public class EmployeePayrollDBConnection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, user, password);
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE employee_payroll SET basic_pay=? WHERE name=?;");
+            preparedStatement.setDouble(1, 5000000);
+            preparedStatement.setString(2, "Vyshnavi");
+            preparedStatement.executeUpdate();
+
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
             ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
